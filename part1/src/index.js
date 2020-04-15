81f4146eaf8a98587700d6016a0745ca273be2b9
古川広一 <ex.kouichi1224@gmail.com>
2020-04-15 20:34:01 +0900

    completed props section

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Hello = () => {
+const Hello = (props) => {
   return (
     <div>
-      <p>Hello world.</p>
+      <p>Hello {props.name}. You are {props.age} years old.</p>
     </div>
   )
 }
 
 const App = () => {
+  const name = 'Peter'
+  const age = 10
+
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello />
-      <Hello />
-      <Hello />
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
     </div>
   )
 }
