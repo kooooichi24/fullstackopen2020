c84373c390c45128398c9d25cbf6cee780a00962
古川広一 <ex.kouichi1224@gmail.com>
2020-04-16 20:59:00 +0900

    refactoring exercise1.5

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -18,9 +18,11 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.parts[0].name} exercise={props.parts[0].exercise} />
-      <Part part={props.parts[1].name} exercise={props.parts[1].exercise} />
-      <Part part={props.parts[2].name} exercise={props.parts[2].exercise} />
+      {props.parts.map((part) => {
+        return (
+          <Part key={part.name} part={part.name} exercise={part.exercise} />
+        )
+      })}
     </div>
   )
 }
