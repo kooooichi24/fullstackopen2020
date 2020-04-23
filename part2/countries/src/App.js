127a87a8b58a0ab3bfe7621ab52ec76f94e18785
古川広一 <ex.kouichi1224@gmail.com>
2020-04-23 22:25:11 +0900

    completed Data for countries step2 of part2.c

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,31 +1,50 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const DisplayCounty = ({countries}) => {
+const DisplayCountry = ({country}) => {
+  const style = {
+    width: '150px',
+    height: '150px'
+  }
+  return (
+    <div>
+      <h2>{country.name}</h2>
+      <div>capital {country.capital}</div>
+      <div>population {country.population}</div>
+      <h2>languages</h2>
+      <ul>
+        {country.languages.map(language => <li key={language.iso639_1}>{language.name}</li>)}
+      </ul>
+      <img src={country.flag} alt={country.name} style={style} />
+    </div>
+  )
+}
+
+const DisplayCountries = ({countries}) => {
+  const [country, setCountry] = useState([])
+
   if (countries.length === 0) {
     return ''
   } else if (countries.length > 10) {
     return <div>Too many matches, specify another filter</div>
   } else if (countries.length > 1) {
-    return countries.map(country => <div key={country.numericCode}>{country.name}</div>)
-  } else {
-    const country = countries[0]
-    const style = {
-      width: '150px',
-      height: '150px'
-    }
     return (
       <div>
-        <h2>{country.name}</h2>
-        <div>capital {country.capital}</div>
-        <div>population {country.population}</div>
-        <h2>languages</h2>
-        <ul>
-          {country.languages.map(language => <li key={language.iso639_1}>{language.name}</li>)}
-        </ul>
-        <img src={country.flag} alt={country.name} style={style} />
+        {countries.map(country => {
+          return (
+            <div key={country.numericCode}>
+              {country.name}
+              <button onClick={() => setCountry([country])}>
+                show
+              </button>
+            </div>
+          )
+        })}
+        {country.length === 1 ? <DisplayCountry country={country[0]} /> : ''}
       </div>
     )
+  } else {
+    return <DisplayCountry country={countries[0]} />
   }
 }
 
@@ -62,7 +81,7 @@ const App = () => {
         find coutries 
         <input value={searchText} onChange={handleTextChange} />
       </div>
-      <DisplayCounty countries={countries} />
+      <DisplayCountries countries={countries} />
     </div>
   )
 }
