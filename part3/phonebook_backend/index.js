70c8bf7a052e40c80afebcacd28747b1378bc10c
古川広一 <ex.kouichi1224@gmail.com>
2020-04-26 18:19:27 +0900

    implemented get person by id about the exercise 3.3

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -27,12 +27,23 @@ let persons = [
 ]
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  res.send('<h1>Phonebook backend app</h1>')
 })
 
 app.get('/api/persons', (req, res) => {
   res.json(persons)
   console.log(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    res.json(person)
+  } else {
+    console.log("can't get person")
+    res.status(404).end()
+  }
 })
 
 app.get('/api/info', (req, res) => {
