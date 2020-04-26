9bef7b748f5a23cd93927855b12a32418124aba1
古川広一 <ex.kouichi1224@gmail.com>
2020-04-26 17:32:51 +0900

    completed lecture code of part3.a

--- part3/lecture_backend/index.js
+++ part3/lecture_backend/index.js
@@ -22,12 +22,52 @@ let notes = [
   }
 ]
 
-app.get('/', (req, res) => {
-  res.send('<h1>hello world</h1>')
-})
+app.use(express.json())
 
 app.get('/api/notes', (req, res) => {
   res.json(notes)
+})
+
+app.get('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const note = notes.find(note => note.id === id)
+  if (note) {
+    res.json(note)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  res.status(204).end()
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/notes', (req, res) => {
+  const body = req.body
+  if (!body.content) {
+    return res.status(400).json({
+      error: 'content missing'
+    })
+  }
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId()
+  }
+  notes = notes.concat(note)
+  console.log(notes)
+  res.json(note)
 })
 
 const PORT = 3001
