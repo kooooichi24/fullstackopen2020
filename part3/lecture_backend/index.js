8357c6e6ec157e6eab8ea802e4e8c80db46f2d4f
古川広一 <ex.kouichi1224@gmail.com>
2020-04-27 21:46:22 +0900

    update code

--- part3/lecture_backend/index.js
+++ part3/lecture_backend/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
+app.use(express.static('build'))
+app.use(cors())
 app.use(express.json())
 
 const requestLogger = (request, response, next) => {
@@ -86,7 +89,7 @@ app.post('/api/notes', (req, res) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
   console.log(`http://localhost:${PORT}`);
