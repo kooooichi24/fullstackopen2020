45cc31b31d6f9b66be2c8d8549712799ba93acc6
古川広一 <ex.kouichi1224@gmail.com>
2020-04-30 13:27:04 +0900

    completed lecture of part3.c

--- part3/lecture_backend/index.js
+++ part3/lecture_backend/index.js
@@ -5,8 +5,9 @@ const app = express()
 const Note = require('./models/note')
 
 app.use(express.static('build'))
-app.use(cors())
 app.use(express.json())
+app.use(cors())
+
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -15,12 +16,8 @@ const requestLogger = (request, response, next) => {
   console.log('------')
   next()
 }
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint'})
-}
-
 app.use(requestLogger)
+
 
 let notes = [
   {
@@ -49,7 +46,7 @@ app.get('/api/notes', (req, res) => {
   })
 })
 
-app.get('/api/notes/:id', (req, res) => {
+app.get('/api/notes/:id', (req, res, next) => {
   Note.findById(req.params.id)
     .then(note => {
       if (note) {
@@ -58,17 +55,7 @@ app.get('/api/notes/:id', (req, res) => {
         res.status(404).end()
       }
     })
-    .catch(error => {
-      console.log(error);
-      res.status(400).send({ error: 'malformatted id' })
-    })
-})
-
-app.delete('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  res.status(204).end()
+    .catch(error => next(error))
 })
 
 app.post('/api/notes', (req, res) => {
@@ -88,7 +75,43 @@ app.post('/api/notes', (req, res) => {
   })
 })
 
+app.put('/api/notes/:id', (req, res, next) => {
+  const body = req.body
+  const note = {
+    content: body.content,
+    important: body.important
+  }
+
+  Note.findByIdAndUpdate(req.params.id, note, { new: true })
+    .then(updatedNote => {
+      res.json(updatedNote.toJSON())
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/notes/:id', (req, res, next) => {
+  Note.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint'})
+}
+// handler of requests with unknown endpoint
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message);
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  next(error)
+}
+// handler of requests with result to errors
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
