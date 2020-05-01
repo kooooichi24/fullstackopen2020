bf34007df0bc589c271661af846ff67732a6f173
古川広一 <ex.kouichi1224@gmail.com>
2020-05-01 14:16:00 +0900

    change project structure

---
+++ part4/lecture_backend/build/precache-manifest.7d5b23207f5ccaaaee77c8192d7904a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f7a5658f27d896f6f3caf8d7f9a9c54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d25264f66c44b02f2f9b",
+    "url": "/static/css/main.7efb19ac.chunk.css"
+  },
+  {
+    "revision": "750bc1731f72297e580b",
+    "url": "/static/js/2.664c2ece.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.664c2ece.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d25264f66c44b02f2f9b",
+    "url": "/static/js/main.2a473122.chunk.js"
+  },
+  {
+    "revision": "17f6e71cf05be418eb3d",
+    "url": "/static/js/runtime-main.b0cd71f4.js"
+  }
+]);
