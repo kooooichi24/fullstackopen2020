3c6fcaa315b66753e96e0408ea964c74b0252d12
古川広一 <ex.kouichi1224@gmail.com>
2020-05-01 18:00:37 +0900

    completed the exercise 4.2 about changing project structure in bloglist_app

---
+++ part4/bloglist_backend/controllers/blogs.js
@@ -0,0 +1,29 @@
+const blogRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogRouter.get('/', (req, res) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      res.json(blogs.map(blog => blog.toJSON()))
+    })
+})
+
+blogRouter.post('/', (req, res) => {
+  const body = req.body
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  })
+
+  blog
+    .save()
+    .then(savedBlog => {
+      res.json(savedBlog.toJSON())
+    })
+})
+
+module.exports = blogRouter
