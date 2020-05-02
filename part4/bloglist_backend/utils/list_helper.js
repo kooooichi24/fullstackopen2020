fe3d5a01e12fc8c1ace608a040a7ea41f9465bb1
古川広一 <ex.kouichi1224@gmail.com>
2020-05-02 13:35:11 +0900

    completed the exercise 4.5

--- part4/bloglist_backend/utils/list_helper.js
+++ part4/bloglist_backend/utils/list_helper.js
@@ -11,7 +11,23 @@ const totalLikes = (blogs) => {
     : blogs.map(blog => blog.likes).reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  blogs = blogs.map(blog => {
+    delete blog._id
+    delete blog.__v
+    delete blog.url
+    return blog
+  })
+  const reducer = (max, item) => {
+    return max.likes > item.likes ? max : item
+  }
+  return blogs.length === 0
+    ? []
+    : blogs.reduce(reducer, 0)
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
