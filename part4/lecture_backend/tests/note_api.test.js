8f0b5e41126b6fb8f16d9b70c16583ed52347b34
古川広一 <ex.kouichi1224@gmail.com>
2020-05-05 20:17:04 +0900

    completed Creating_a_new_note in part4.3

--- part4/lecture_backend/tests/note_api.test.js
+++ part4/lecture_backend/tests/note_api.test.js
@@ -5,6 +5,8 @@ const app = require('../app')
 const api = supertest(app)
 
 const Note = require('../models/note')
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
 
 beforeEach(async () => {
   await Note.deleteMany({})
@@ -126,6 +128,60 @@ describe('deletion of a note', () => {
     const contents = notesAtEnd.map(r => r.content)
 
     expect(contents).not.toContain(noteToDelete.content)
+  })
+})
+
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const userAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'koichi furukawa',
+      name: 'kooooichi24',
+      password: 'password'
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const userAtEnd = await helper.usersInDb()
+    expect(userAtEnd).toHaveLength(userAtStart.length + 1)
+
+    const usernames = userAtEnd.map(u => u.username)
+    expect(usernames).toContain(newUser.username)
+  })
+
+  test('creation fails with proper statuscode and message if username already taken', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'root',
+      name: 'rt',
+      password: 'rtrt'
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    expect(result.body.error).toContain('`username` to be unique')
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length)
   })
 })
 
