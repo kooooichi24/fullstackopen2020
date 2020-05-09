e2cbabfeec6d54a5a6cf469022de8c3bae48a7df
古川広一 <ex.kouichi1224@gmail.com>
2020-05-09 21:45:02 +0900

    completed the exercise 5.1

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,15 +1,65 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
   }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+  
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch(exception) {
+      console.log(exception)
+    }
+  }
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value)
+  }
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value)
+  }
+
+  const loginForm = () => (
+    <div>
+      <h2>log in to application</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input type="text" value={username} name="Username" onChange={handleUsernameChange} />
+        </div>
+        <div>
+          password
+          <input type="text" value={password} name="Password" onChange={handlePasswordChange} />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  )
+
+  if (user === null) {
+    return (
+      loginForm()
+    )
+  }
 
   return (
     <div>
