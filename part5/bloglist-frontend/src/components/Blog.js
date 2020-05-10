e44acead598547fcf7b18003a3fa7e7ba98aea5f
古川広一 <ex.kouichi1224@gmail.com>
2020-05-10 16:03:14 +0900

    toggle blog info

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,35 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [ visible, setVisible ] = useState(false)
+
+  const button_text = visible ? "hide" : "view"
+  const viewInfo = { display: visible ? '' : 'none' }
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title}
+        <button onClick={toggleVisibility}>{button_text}</button>
+      </div>
+      <div style={viewInfo}>
+        <div>{blog.author}</div>
+        <div>{blog.likes}</div>
+        <div>{blog.url}</div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
