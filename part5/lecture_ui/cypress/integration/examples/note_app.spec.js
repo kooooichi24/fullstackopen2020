76deb85c74c9c5bff2ecd6b502388939668eb655
古川広一 <ex.kouichi1224@gmail.com>
2020-05-17 16:31:51 +0900

    install eslint-plugin-cypress

--- part5/lecture_ui/cypress/integration/examples/note_app.spec.js
+++ part5/lecture_ui/cypress/integration/examples/note_app.spec.js
@@ -1,7 +1,19 @@
 describe('Note app', function() {
-  it('front page can be opende', function() {
+  beforeEach(function() {
     cy.visit('http://localhost:3000')
+  })
+
+  it('front page can be opende', function() {
     cy.contains('Notes')
     cy.contains('Note app, Department of Computer Science, University of Helsinki 2020')
+  })
+
+  it('login form can be opened', function() {
+    cy.contains('login').click()
+    cy.get('#username').type('root')
+    cy.get('#password').type('sekret')
+    cy.get('#login-button').click()
+
+    cy.contains('logged in')
   })
 })
--- part5/lecture_ui/src/components/LoginForm.js
+++ part5/lecture_ui/src/components/LoginForm.js
@@ -6,13 +6,13 @@ const LoginForm = ({ handleUsernameChange, handlePasswordChange, handleLogin, us
     <form onSubmit={handleLogin}>
       <div>
         username
-        <input type="text" value={username} name="Username" onChange={handleUsernameChange} />
+        <input id='username' type="text" value={username} name="Username" onChange={handleUsernameChange} />
       </div>
       <div>
         password
-        <input type="text" value={password} name="Password" onChange={handlePasswordChange} />
+        <input id='password' type="text" value={password} name="Password" onChange={handlePasswordChange} />
       </div>
-      <button type="submit">login</button>
+      <button id='login-button' type="submit">login</button>
     </form>
   )
 }
