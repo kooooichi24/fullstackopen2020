39e7c2b0c02ab3dc171a1c5a8f6e4c5a55f15ab1
古川広一 <ex.kouichi1224@gmail.com>
2020-06-02 23:13:35 +0900

    router

--- part7/router-notes/src/index.js
+++ part7/router-notes/src/index.js
@@ -1,7 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+import {
+  BrowserRouter as Router,
+  Switch, Route, Link
+} from 'react-router-dom'
 
-ReactDOM.render(
-  <div>Hello world</div>,
-  document.getElementById('root')
-);
+
+const Home = () => (
+  <div> <h2>TKTL notes app</h2> </div>
+)
+
+const Notes = () => (
+  <div> <h2>Notes</h2> </div>
+)
+
+const Users = () => (
+  <div> <h2>Users</h2> </div>
+)
+
+
+const App = () => {
+  const padding = {
+    padding: 5
+  }
+
+  return (
+    <Router>
+      <div>
+        <Link style={padding} to='/'>home</Link>
+        <Link style={padding} to='/notes'>notes</Link>
+        <Link style={padding} to='/users'>users</Link>
+      </div>
+
+      <switch>
+        <Route path='/notes'>
+          <Notes />
+        </Route>
+        <Route path="/users">
+          <Users />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </switch>
+
+      <div>
+        <i>Note app, Department of Computer Science 2020</i>
+      </div>
+    </Router>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
