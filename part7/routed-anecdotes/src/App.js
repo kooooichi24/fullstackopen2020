db1aa5564cf12c44266be009799c5645215bafae
kooooichi24 <ex.kouichi1224@gmail.com>
2020-06-04 20:02:25 +0900

    What is the best way to use spread syntax at 7.6?

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -96,9 +96,9 @@ const CreateNew = (props) => {
     <div>
       <h2>create a new anecdote</h2>
       <form onSubmit={handleSubmit}>
-        <div>content <input {...content} /></div>
-        <div>author <input {...author} /></div>
-        <div>url for more info <input {...info} /></div>
+        <div>content <input type={content.type} value={content.value} onChange={content.onChange}/></div>
+        <div>author <input type={author.type} value={author.value} onChange={author.onChange}/></div>
+        <div>url for more info <input type={info.type} value={info.value} onChange={info.onChange}/></div>
         
         <button type='submit'>create</button>
         <button type='reset' onClick={handleReset}>reset</button>
