6032559eb078d0d52ec085a6285d54c8f0efaeb8
kooooichi24 <ex.kouichi1224@gmail.com>
2020-07-10 23:52:32 +0900

    リファクタリングしたお

--- part8/apollo-client/src/App.js
+++ part8/apollo-client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { gql, useQuery } from '@apollo/client';
 
+import Persons from './components/Persons';
+
 const ALL_PERSONS = gql`
 query {
   allPersons {
@@ -10,19 +12,6 @@ query {
   }
 }
 `
-
-const Persons = ({ persons }) => {
-  return (
-    <div>
-      <h2>Persons</h2>
-      {persons.map(p => 
-        <div key={p.name}>
-          {p.name} {p.phone}
-        </div>
-      )}
-    </div>
-  )
-}
 
 const App = () => {
   const result = useQuery(ALL_PERSONS)
