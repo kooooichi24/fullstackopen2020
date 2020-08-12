20e15c08e1d49ccbe7c25708bb0ff7bd94327dd4
kooooichi24 <ex.kouichi1224@gmail.com>
2020-08-12 23:52:26 +0900

    8.d Updating cache, revisited

--- part8/apollo-client/src/components/PersonForm.js
+++ part8/apollo-client/src/components/PersonForm.js
@@ -8,10 +8,18 @@ const PersonForm = ({ setError }) => {
   const [ street, setStreet ] = useState('');
   const [ city, setCity] = useState('');
   const [ createPerson ] = useMutation(CREATE_PERSON, {
-    
-    refetchQueries: [ { query: ALL_PERSONS }],
     onError: (error) => {
       setError(error.graphQLErrors[0].message);
+    },
+    update: (store, response) => {
+      const dataInStore = store.readQuery({ query: ALL_PERSONS })
+      store.writeQuery({
+        query: ALL_PERSONS,
+        data: {
+          ...dataInStore,
+          allPersons: [ ...dataInStore.allPersons, response.data.addPerson ]
+        }
+      })
     }
   });
 
